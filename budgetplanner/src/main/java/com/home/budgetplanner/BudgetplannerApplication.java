01a5ec0ae95ff6e94bd069f95f2cf3efd6cbbd65
hortegag <hog_andy@hotmail.com>
2018-07-28 22:43:19 -0500

    Configuration with spring boot

---
+++ budgetplanner/src/main/java/com/home/budgetplanner/BudgetplannerApplication.java
@@ -0,0 +1,12 @@
+package com.home.budgetplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetplannerApplication.class, args);
+	}
+}
